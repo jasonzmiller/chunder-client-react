e9b2033c09c6ef63e719971db6c9486f82babd38
chadcomiter <chadcomiter@gmail.com>
2021-04-11 15:17:31 -0400

    Profile Page User Specific

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,25 +1,49 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const Profile = () => {
-    const getUser = () => {
+export default class Profile extends React.Component {
+    
+    state = {
+       users: []
+    }
+
+    /*
+    getUser(){
         axios({
             method: "get",
             withCredentials: true,
             url: "http://localhost:4000/user"
-        }).then((res) => console.log(res));
+        }).then((res) => {
+            console.log(res)
+            const user = res.data;
+            this.setState({ user })
+        });
+    }
+    */
+
+    componentDidMount(){
+        axios({
+            method: "get",
+            withCredentials: true,
+            url: "http://localhost:4000/user"
+        })
+            .then(res => {
+                const users = res.data;
+                this.setState({ users });
+                console.log(res.data)
+                console.log(users.username)
+            })
     }
 
     
-    
+    render(){
     return (
         <div>
-                <h1>Get User</h1>
-                <button onClick={getUser}>Get User</button>
-            </div>
+            <h1>
+                {this.state.users.username}
+            </h1>
+        </div>
     )
 
-
+    }
 }
-
-export default Profile
