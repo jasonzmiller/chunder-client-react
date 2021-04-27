14eea80e09839847a85829b316250db7c7b44ed7
chadcomiter <chadcomiter@gmail.com>
2021-04-26 20:15:56 -0400

    Working Mountains For User

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import mountainService from '../services/mountain-service'
+import { findMountainById } from '../services/mountain-service';
+
 
 export default class Profile extends React.Component {
     
     state = {
        users: [],
-       mountainsForUser: []
+       mountainsForUser: [],
+       mountainObjects: []
     }
 
     /*
@@ -32,12 +34,16 @@ export default class Profile extends React.Component {
         })
             .then(res => {
                 const users = res.data;
+                const mountainsForUser = users.mountains;
                 this.setState({ users });
+                this.setState({ mountainsForUser })
                 console.log(res.data)
                 console.log(users.username)
+                console.log(mountainsForUser)
             })
     }
 
+    
     
     render(){
     return (
@@ -48,19 +54,35 @@ export default class Profile extends React.Component {
                 </div>
             </div>
             <div>
-                <h3>Here are your favorites:</h3>
+                <button
+                onClick={() => this.state.mountainsForUser.map((mid) => {
+                    console.log("Finding" + mid);
+                    findMountainById(mid)
+                        .then(res => {
+                            console.log(res)
+                            const mtnToAdd = res;
+                            this.setState({
+                                mountainObjects: [
+                                    ...this.state.mountainObjects,
+                                    mtnToAdd
+                                ]
+                            })
+                            console.log(this.state.mountainObjects)
+                        })
+                })}
+                >Find Mountains</button>
             </div>
             <div className="row">
                 {
-                    this.state.mountainsForUser &&
-                    this.state.mountainsForUser.map(mountain => 
+                    this.state.mountainObjects &&
+                    this.state.mountainObjects.map(mountain => 
                         <div className="col-4">
                             <ul className="list-group">
                                 <li className="list-group-item">
                                     <img style={{height: "20rem", width: "26rem"}} src="https://images.unsplash.com/photo-1532124957326-34c5605398?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80"></img>
                                 </li>
                                 <li className="list-group-item">
-                                    Name: <Link to={`/mountains/${mountain.name}`}>{mountain.name}</Link>
+                                    Name: <Link to={`/mountains/${mountain._id}`}>{mountain.name}</Link>
                                 </li>
                                 <li className="list-group-item">
                                     City: {mountain.city}
